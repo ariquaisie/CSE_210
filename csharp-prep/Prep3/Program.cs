ee73b32aded16524e5fbd444ba21ecc47d496021
ariquaisie <ariellaqueenbay143@gmail.com>
2026-06-13 23:39:14 -0600

    Update Program.cs
    assignment submission

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        do {
+            Console.Write("What is your guess?: ");
+            string guessstring = Console.ReadLine();
+            guess = int.Parse(guessstring);
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            } else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the number!");
+            }
+        } while (guess != magic);
     }
 }
