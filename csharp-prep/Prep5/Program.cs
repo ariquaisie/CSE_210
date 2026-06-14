e9db0c10125856ae74e23388490dfd6c5ec3327a
ariquaisie <ariellaqueenbay143@gmail.com>
2026-06-14 00:43:56 -0600

    Update Program.cs
    sorry it's late. After fixing the problems in Prep4. I had to check if this one to had problems and I realized it did so I looked at your code to guide me to fix it. But I completed it long time ago in onlinegdb

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber();
+        int SquaredNumber = SquareNumber(UserNumber);
+        int birthyear; PromptUserBirthYear(out birthyear);
+        DisplayResult(UserName, SquaredNumber, birthyear);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    
+    static void PromptUserBirthYear(out int birthyear)
+    {
+        Console.Write($"Please enter the year you were born: ");
+        birthyear = int.Parse(Console.ReadLine());
+
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square, int birthyear)
+    {
+        int thisyear = 2026;
+        int age = thisyear-birthyear;
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will turn {age} years old this year.");
     }
 }
