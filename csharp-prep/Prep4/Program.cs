4819e17bfde784cc310a7bfd01604aa454d8bfa7
ariquaisie <ariellaqueenbay143@gmail.com>
2026-06-14 00:10:06 -0600

    Update Program.cs
    I finished early I was just debugging it because it wasn't working but when I first did it it was working and I wasn't sure what was wrong with it.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> number = new List<int> ();
+        int addnumber = 1;
+
+        do {
+            Console.Write("Enter a number to add. Enter 0 to finish: ");
+            string addnumberstring = Console.ReadLine();
+            addnumber = int.Parse(addnumberstring);
+
+            if (addnumber != 0 )
+            {
+                number.Add (addnumber);
+            } 
+        } while (addnumber != 0);
+
+        int add = 0;
+        foreach (int total in number)
+        {
+            add += total;
+        }
+        Console.WriteLine($"The sum is: {add}");
+        
+        float average = ((float)add) / number.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largestnumber = number[0];
+        foreach (int total in number)
+        {
+            if (total > largestnumber)
+            {
+                largestnumber = total;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestnumber}");
     }
 }
