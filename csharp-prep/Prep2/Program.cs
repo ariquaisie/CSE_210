cdda50c15b7a8b39e3777606fda1785b874ea3ac
ariquaisie <ariellaqueenbay143@gmail.com>
2026-06-13 23:00:36 -0600

    Update Program.cs
    assignment submission

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade?: ");
+        string gradestring = Console.ReadLine();
+        int grade = int.Parse(gradestring);
+
+        if (grade >= 90)
+        {
+            string lettergrade = "A";
+            Console.WriteLine($"You have an {lettergrade}");
+        } else if (grade < 90 && grade >= 80)
+        {
+            string lettergrade = "B";
+            Console.WriteLine($"You have an {lettergrade}");
+        } else if (grade < 80 && grade >= 70)
+        {
+            string lettergrade = "C";
+            Console.WriteLine($"You have an {lettergrade}");
+        } else if (grade < 70 && grade >= 60)
+        {
+            string lettergrade = "D";
+            Console.WriteLine($"You have an {lettergrade}");
+        } else
+        {
+            string lettergrade = "F";
+            Console.WriteLine($"You have an {lettergrade}");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! Y0u failed this class. You would have to retake it if it is a requirement for your major!");
+        }
+
     }
 }
